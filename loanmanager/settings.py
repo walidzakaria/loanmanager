b21e4c3118237a409cf1702a29a1e83ee2302776
walidzakaria <walidpiano@yahoo.com>
2024-04-16 19:46:36 +0200

    add ar

--- loanmanager/settings.py
+++ loanmanager/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    
+    'django.middleware.locale.LocaleMiddleware',
     # 'corsheaders.middleware.CorsMiddleware',
     # 'django_user_agents.middleware.UserAgentMiddleware',    
 ]
